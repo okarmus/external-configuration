cc67a451512f0a62d5170d8af58fa9dc33a38d08
mateusz <mateusz.okarmus@gmail.com>
2016-12-06 11:41:36 +0100

    final commit

--- message-service/src/main/java/org/okarmus/controller/MessageController.java
+++ message-service/src/main/java/org/okarmus/controller/MessageController.java
@@ -1,5 +1,6 @@
 package org.okarmus.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
+    @Value("${message}")
+    private String message;
+
     @RequestMapping
     public String getHello() {
-        return "hello";
+        return message;
     }
 }
