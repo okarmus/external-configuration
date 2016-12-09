907f8bae05c787b0f5fc8aaef31debd53c5afe5b
mateusz <mateusz.okarmus@gmail.com>
2016-12-09 12:23:56 +0100

    Add eureka discovery service

--- eureka-service/src/main/java/org/okarmus/EurekaServiceApplication.java
+++ eureka-service/src/main/java/org/okarmus/EurekaServiceApplication.java
@@ -2,12 +2,13 @@ package org.okarmus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class MessageServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(MessageServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 }
